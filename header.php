4670dc5478ae5b977b8b767458ae7ff6e0f940d3
Hafijur Rahman <developer.hafij@gmail.com>
2020-03-06 22:23:13 +0600

    second part by hafij

---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/c1cce69857.js">
+    <script src="https://use.fontawesome.com/c1cce69857.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,182 @@
+<?php include "header.php" ?>
+
+<section>
+	<div class="margin-top" style="margin-top: 150px;">
+		
+	</div>
+</section>
+
+<section class="dashborad">
+	<div class="" style="padding: 0 20px;">
+		<div class="row">
+			<!-- First Card Item Start -->
+			<div class="items col-md-4">
+				<div class="card">
+					<div class="card-action">
+						<div class="card-top-border"></div>
+						<div class="card-bottom-border"></div>
+					</div>
+				 	<div class="row">
+					  	<div class="col-md-6">
+						 	<div class="card-title">
+						  		<h6>Cash Flow</h6>
+							</div>
+						</div>
+						 <div class="col-md-6">
+						  	<i class="fa fa-ellipsis-v"></i>
+						 </div>
+				  	</div>
+				  	<div class="border"></div>
+				  	<div class="card-body">
+						<span>Last 31 days</span>
+						<h2>BDT 0.00</h2>
+						<span>Income</span>
+						<span>Expence</span>
+				  	</div>
+			  	</div>
+			</div>
+			<!-- First Card Item End -->
+
+			<!-- Second Card Item Start -->
+			<div class="items col-md-4">
+				<div class="card">
+					<div class="card-action">
+						<div class="card-top-border"></div>
+						<div class="card-bottom-border"></div>
+					</div>
+				 	<div class="row">
+					  	<div class="col-md-6">
+						 	<div class="card-title">
+						  		<h6>Dashboard</h6>
+							</div>
+						</div>
+						 <div class="col-md-6">
+						  	<i class="fa fa-ellipsis-v"></i>
+						 </div>
+				  	</div>
+				  	<div class="border"></div>
+				  	<div class="card-body">
+						<span>Last 31 days</span>
+						<h2>BDT 0.00</h2>
+						<span>Income</span>
+						<span>Expence</span>
+						<span>Last 31 days</span>
+						<h2>BDT 0.00</h2>
+						<span>Income</span>
+				  	</div>
+			  	</div>
+			</div>
+			<!-- Second Card Item End -->
+
+			<!-- Third Card Item Start -->
+			<div class="items col-md-4">
+				<div class="card card-hover">
+				  	<div class="card-body add-card">
+						<i class="fa fa-plus-circle"></i>
+						<h5>Add Card</h5>
+				  	</div>
+			  	</div>
+			</div>
+			<!-- Third Card Item End -->
+
+			<!-- First Card Item Start -->
+			<div class="items col-md-4">
+				<div class="card">
+					<div class="card-action">
+						<div class="card-top-border"></div>
+						<div class="card-bottom-border"></div>
+					</div>
+				 	<div class="row">
+					  	<div class="col-md-6">
+						 	<div class="card-title">
+						  		<h6>Express Structure</h6>
+							</div>
+						</div>
+						<div class="col-md-2"></div>
+						<div class="col-md-2 record" style="padding-right: 0;">
+							<p>Email</p>
+						</div>
+						<div class="col-md-2">
+						  	<i class="fa fa-ellipsis-v" style="padding: 15px;"></i>
+						</div>
+				  	</div>
+				  	<div class="border"></div>
+				  	<div class="card-body">
+						<i class="fa fa-envelope" style="font-size: 75px; color: #ddd; text-align: center;"></i>
+						<p>No longer Express Structure available right now </p>
+				  	</div>
+			  	</div>
+			</div>
+			<!-- First Card Item End -->
+
+			<!-- Second Card Item Start -->
+			<div class="items col-md-4">
+				<div class="card">
+					<div class="card-action">
+						<div class="card-top-border"></div>
+						<div class="card-bottom-border"></div>
+					</div>
+				 	<div class="row">
+					  	<div class="col-md-6">
+						 	<div class="card-title">
+						  		<h6>Last Record</h6>
+							</div>
+						</div>
+						 <div class="col-md-6">
+						  	<i class="fa fa-ellipsis-v"></i>
+						 </div>
+				  	</div>
+				  	<div class="border"></div>
+				  	<div class="card-body">
+						<span>Last 31 days</span>
+						<h2>BDT 0.00</h2>
+						<span>Income</span>
+						<span>Expence</span>
+				  	</div>
+			  	</div>
+			</div>
+			<!-- Second Card Item End -->
+
+			<!-- Third Card Item Start -->
+			<div class="items col-md-4">
+				<div class="card">
+					<div class="card-action">
+						<div class="card-top-border"></div>
+						<div class="card-bottom-border"></div>
+					</div>
+				 	<div class="row">
+					  	<div class="col-md-6">
+						 	<div class="card-title">
+						  		<h6>Admin</h6>
+							</div>
+						</div>
+						 <div class="col-md-6">
+						  	<i class="fa fa-ellipsis-v"></i>
+						 </div>
+				  	</div>
+				  	<div class="border"></div>
+				  	<div class="card-body">
+						<span>Last 31 days</span>
+						<h2>BDT 0.00</h2>
+						<span>Income</span>
+						<span>Expence</span>
+				  	</div>
+			  	</div>
+			</div>
+			<!-- Third Card Item End -->
+
+
+
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="margin-top" style="margin-top: 150px;">
+		
+	</div>
+</section>
+ 
+
+
+<?php include "footer.php" ?> 
