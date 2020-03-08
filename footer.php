0b55c0dc5ce53660ef301583a6b0821f1f38b256
Hafijur Rahman <developer.hafij@gmail.com>
2020-03-08 14:38:52 +0600

    Drang and Drop design

--- footer.php
+++ footer.php
@@ -1,9 +1,21 @@
-
-	
+ 	
 	<!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+      $( function() {
+        /*$( "#draggable , #draggable, #draggable , #draggable, #draggable , #draggable" ).draggable();*/
+        $( "#sortable, #sortable,#sortable, #sortable,#sortable, #sortable" ).sortable();
+        $( "#sortable, #sortable, #sortable, #sortable" ).disableSelection();
+      } );
+    </script>
+    
+
   </body>
 </html>
--- header.php
+++ header.php
@@ -4,15 +4,21 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/c1cce69857.js">
+    <!-- Jquery UI CSS -->
+    <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"> -->
+
     <script src="https://use.fontawesome.com/c1cce69857.js"></script>
 
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
     <title>DashBoard Design</title>
+
+
   </head>
   <body>
